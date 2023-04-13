53fd3a4fffe5825f745c70a4ac0d14ccbcb0bf0e
Aliona83 <alionajanusko@gmail.com>
2023-04-13 11:36:00 +0000

    Change name of app in settings

--- recipe_book/settings.py
+++ recipe_book/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['app-recipe.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['recipe-block.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
