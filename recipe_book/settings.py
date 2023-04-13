ef39bec17197886e291ec5ac738f543e03efca29
Aliona83 <alionajanusko@gmail.com>
2023-04-13 12:56:57 +0000

    fixed db settings.py

--- recipe_book/settings.py
+++ recipe_book/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'recipe_book.wsgi.application'
 # }
 
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
